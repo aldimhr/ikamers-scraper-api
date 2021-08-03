79486b3e1d0578c3208c96f830162ce742a6c798
Aldimhr <dirikuin16@gmail.com>
2021-08-03 23:48:18 +0700

    add jontewks/puppeteer

--- routing/product.js
+++ routing/product.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controller/Product');
-
 router.get('/v1/shopee/product', productController.scrape);
-
 module.exports = router;
